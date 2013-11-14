1db8c76d16683581ae40a0c8da9de49f9c2723a0
Ramon Navarro Bosch <ramon.nb@gmail.com>
2013-11-14 23:21:41 +0100

    Styling, globalstatus, interfaces, ...

--- plonetheme/barceloneta/interfaces.py
+++ plonetheme/barceloneta/interfaces.py
@@ -1,5 +1,6 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
+
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer and a plone skin marker.
     """
