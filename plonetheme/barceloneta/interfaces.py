941346c24956201adab02dba4cc0c1028e690335
Rok Garbas <rok@garbas.si>
2013-11-27 20:35:24 +0100

    in short: integrate with mockup barceloneta bundle
    - add static resources
    - fix absolute url to relative which fixed theme resources url
    - moved barceloneta folder to theme
    - removed bootstrap copy since its being now correctly pulled from mockup
    
    that's for now, more commin'

--- plonetheme/barceloneta/interfaces.py
+++ plonetheme/barceloneta/interfaces.py
@@ -1,6 +1,7 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 
 
-class IThemeSpecific(IDefaultPloneLayer):
-    """Marker interface that defines a Zope 3 browser layer and a plone skin marker.
+class IBarcelonetaLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer and a plone skin
+       marker.
     """
