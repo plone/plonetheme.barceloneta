1d161ed57f416300e41dea079d125b0e959057d6
Ramon Navarro Bosch <ramon.nb@gmail.com>
2014-11-01 20:11:00 +0000

    back to using browser layer because we are testing on upgade

---
+++ plonetheme/barceloneta/interfaces.py
@@ -0,0 +1,7 @@
+from plone.theme.interfaces import IDefaultPloneLayer
+
+
+class IBarcelonetaLayer(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 browser layer and a plone skin
+       marker.
+    """
