15159e5f12e6557f171f886eb28bf7ed8867dd94
Maurits van Rees <maurits@vanrees.org>
2018-02-05 10:15:09 +0100

    Preparing release 1.6.24
    [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.6.24.dev0'
+version = '1.6.24'
 
 long_description = (
     read('README.rst') + '\n' +
