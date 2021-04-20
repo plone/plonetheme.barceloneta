5cbabe728511fc42a7064315b9376c3a8d88477c
esteele <eric@esteele.net>
2021-04-20 10:46:09 -0400

    Back to development: 3.0.0a2
    [ci skip]

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "3.0.0a1"
+version = "3.0.0a2.dev0"
 
 long_description = read("README.rst") + "\n" + read("CHANGES.rst") + "\n"
 
