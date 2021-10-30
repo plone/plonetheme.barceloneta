7043d081f3508474be752aa1592b64b5d54ba902
Peter Holzer <peter.holzer@agitator.com>
2021-10-30 17:29:01 +0200

    Mark IBarcelonetaLayer deprecated

--- plonetheme/barceloneta/interfaces.py
+++ plonetheme/barceloneta/interfaces.py
@@ -1,6 +1,7 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from zope.deprecation import deprecated
 
-
+@deprecated("Use IDefaultPloneLayer directly")
 class IBarcelonetaLayer(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer and a plone skin
     marker.
