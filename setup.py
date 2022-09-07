2af815b2cdaa80a6c976a50320125b5a4c268b6b
Maurits van Rees <maurits@vanrees.org>
2022-09-07 21:45:39 +0200

    Added Python 3.10 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     keywords="web zope plone theme",
     author="Plone Foundation",
