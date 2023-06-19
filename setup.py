8bdc1f94ebb05d48313c69703260426703d5466e
Peter Mathis <peter.mathis@kombinat.at>
2023-06-19 10:56:55 +0200

    Back to development: 3.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = "3.0.3"
+version = "3.0.4.dev0"
 
 long_description = read("README.rst") + "\n" + read("CHANGES.rst") + "\n"
 
