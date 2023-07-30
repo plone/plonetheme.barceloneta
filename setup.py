f027d0b7cc29a71e19feca1ce763b0f7b3b65cc3
Peter Mathis <peter.mathis@kombinat.at>
2023-07-30 23:36:41 +0200

    Back to development: 3.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.1.4"
+version = "3.1.5.dev0"
 
 long_description = f"{Path('README.md').read_text()}\n{Path('CHANGES.md').read_text()}"
 
