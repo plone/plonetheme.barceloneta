92d505cb7a7d3bbc7cbf1be719480199282507ba
Maurits van Rees <maurits@vanrees.org>
2024-08-01 19:38:53 +0200

    Back to development: 3.2.0a6
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "3.2.0a5"
+version = "3.2.0a6.dev0"
 
 long_description = f"{Path('README.md').read_text()}\n{Path('CHANGES.md').read_text()}"
 
